9525508a756e86c6f2d0c737c476c1804d003d48
jessiyang1996 <jessiyang1996@gmail.com>
2023-11-25 09:53:24 -0600

    addToCollection fuction and test complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,31 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection=[]; // an array of objects
 
+function addToCollection(collection, title, artist, yearPublished){
+  console.log( 'IN ADD TO COLLECTION FUNCTION' );
+  collection = {}; // Telling the computer that collection is an object
+  collection.title = title;
+  collection.artist = artist;
+  collection.yearPublished = yearPublished;
+  myCollection.push(collection);
+  return collection; // return newly created object
+}
 
-
-
+console.log('========== addToCollection TEST ==========')
+let sikKObject;
+console.log(addToCollection(sikKObject, 'HEADLINER', 'SIK-K', 2020));
+let btsObject;
+console.log(addToCollection(btsObject, 'Lover Yourself Answer', 'BTS', 2018));
+let highlightObject;
+console.log(addToCollection(highlightObject, 'Fiction and Fact', 'Beast', 2011 ));
+let dmtnObject;
+console.log(addToCollection(dmtnObject, 'State of Emergency', 'DMTN', 2012));
+let taeyeonObject;
+console.log(addToCollection(taeyeonObject, 'INVU', 'Taeyeon', 2022));
+let winnerObject;
+console.log(addToCollection(winnerObject, '2014 S/S', 'Winner', '2014'));
+console.log('LOG OF myCollection ARRAY:', myCollection);
 
 
 
